fc47576311b80efdabd343d5ea4ea7a9bb4d47ce
Tag <Tag@Tags-MacBook-Pro.local>
2017-10-08 13:34:20 -0400

    Laravel project setup, gutted a few things

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Memory Game</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+
+  <body>
+    <h1>Memory Game</h1>
+    <footer>
+      <script src="/js/app.js"></script>
+    </footer>
+  </body>
+</html>
